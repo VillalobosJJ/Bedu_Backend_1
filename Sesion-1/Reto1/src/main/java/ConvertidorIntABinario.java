753a9738356208a5828a48d6f67db030a20a7002
Javier Villalobos <112444122+VillalobosJJ@users.noreply.github.com>
2023-03-01 13:41:25 -0700

    Ejercicios

---
+++ Sesion-1/Reto1/src/main/java/ConvertidorIntABinario.java
@@ -0,0 +1,7 @@
+class ConvertidorIntABinario{
+    public static void main(String[] args) {
+        int entero = 2;
+        String binario = Integer.toBinaryString(entero);
+        System.out.println("El numero "+entero +" en binario es:" + binario);
+    }
+}
---
+++ Sesion-1/Reto2/src/main/java/Suma100Numeros.java
@@ -0,0 +1,9 @@
+class Suma100Numeros{
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 100; i++){
+            total = total + i;
+        }
+        System.out.println(total);
+    }
+}
